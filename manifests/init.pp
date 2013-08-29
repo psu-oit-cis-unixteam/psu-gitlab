7b0e051d40a8152dbbf34566529c0b078128cb7e
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2013-08-29 11:03:16 -0700

    some rough framework.

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class gitlab {
+
+  include gitlab::setup
+
+  #service { 'gitlab':
+  #  ensure => running,
+  #  ...
+  #  }
+
+    
+}
---
+++ manifests/setup.pp
@@ -0,0 +1,11 @@
+class gitlab::setup {
+
+  #user { 'git':
+  #  ensure => present,
+  #  ...
+  #  }
+
+  # required packages
+
+    
+}
